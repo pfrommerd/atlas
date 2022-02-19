374703e079d249c8dd1a09a59070541c6340bd49
Daniel Pfrommer <dan.pfrommer@gmail.com>
2022-02-19 11:23:22 -0500

    Fixed tests

--- src/vm/test.rs
+++ src/vm/test.rs
@@ -52,6 +52,6 @@ fn test_prelude_end_to_end() {
     future::block_on(exec.run(async {
         let res = machine.force(handle).await.unwrap();
         let val = res.as_record().unwrap();
-        assert_eq!(val.len(), 8);
+        assert_eq!(val.len(), 12);
     }));
 }
